17a55b63e87eb9e490c43cbf95547f6af25e9f35
skruk <seweryn.kruk@neosystem.pl>
2024-11-25 21:05:33 +0100

    Angular update

--- MonitorPlatform/src/app/app.component.ts
+++ MonitorPlatform/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MqttService, IMqttMessage } from 'ngx-mqtt';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MonitorPlatform';
+  messages: string[] = [];
+
+  constructor(private mqttService: MqttService) { }
+
+  ngOnInit(): void {
+    this.mqttService.observe('station/IX/1/lcd').subscribe((message: IMqttMessage) => {
+      const msg = message.payload.toString();
+      this.messages.push(msg);
+      console.log('Received message:', msg);
+    });
+  }
+  sendMessage(): void {
+    const payload = 'Hello MQTT from Angular!';
+    this.mqttService.unsafePublish('test/topic', payload, { qos: 1 });
+    console.log('Message sent:', payload);
+  }
 }
--- MonitorPlatform/src/app/app.module.ts
+++ MonitorPlatform/src/app/app.module.ts
@@ -4,13 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MqttModule, IMqttServiceOptions } from 'ngx-mqtt';
+
+
+const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname: '127.0.0.1',
+  port: 1883,
+  protocol: 'wss'
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MqttModule.forRoot(MQTT_SERVICE_OPTIONS)
   ],
   providers: [],
   bootstrap: [AppComponent]
